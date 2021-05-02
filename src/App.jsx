270c183388574a3673963b1d0e407403ebed1314
Otto Sanchez <otto.sancheza@gmail.com>
2021-05-02 07:42:49 -0500

    updated to show phone numbers

--- src/App.jsx
+++ src/App.jsx
@@ -24,12 +24,30 @@ class App extends React.Component {
 
   }
   render () {
-    console.log(this.state);
+    let rows = this.state.data.map((item) => {
+      return (
+        <tr>
+          <td>{item.phoneNumber}</td>
+          <td>{item.vanityWords.join('| ')}</td>
+          <td>{item.timestamp}</td>
+        </tr>
+      )
+    });
     return (
       <div className="App">
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1>Hello from V2</h1>
+          <h1>Vanity Numbers App</h1>
+          <table>
+            <thead>
+                <tr>
+                    <th colspan="3">Vanity Numbers Last 5 Callers</th>
+                </tr>
+            </thead>
+            <tbody>
+                {rows}
+            </tbody>
+          </table>
         </header>
       </div>
     );
